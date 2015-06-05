5eee95a219762dca77b5fd1e159d2b8a738b929a
André Santos <andrewskad@gmail.com>
2015-06-06 00:22:45 +0100

    Add movement to Enemy

--- src/Entity/Enemy.java
+++ src/Entity/Enemy.java
@@ -3,12 +3,13 @@ package Entity;
 import Core.Board;
 import java.awt.Color;
 
-public class Enemy extends Entity {
+public class Enemy extends Entity implements Runnable {
 
     Color color;
 
     public Enemy(int x, int y, Board board) {
         super(x, y, Color.RED, board);
+        new Thread(this).start();
     }
 
     @Override
@@ -21,4 +22,26 @@ public class Enemy extends Entity {
         return true;
     }
 
+    private void move() {
+        if (y < board.getPlayer().getY()) {
+            down();
+        } else if (y > board.getPlayer().getY()) {
+            up();
+        } else if (x < board.getPlayer().getX()) {
+            right();
+        } else if (x > board.getPlayer().getX()) {
+            left();
+        }
+        run();
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            move();
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
