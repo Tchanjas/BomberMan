c1e20b7b45b52585e0a01cd932d7c7b596d99ab4
André Santos <andrewskad@gmail.com>
2015-06-06 18:39:35 +0100

    Fix explosion so it can't hit several bricks on the same direction

--- src/Temp/Bomb.java
+++ src/Temp/Bomb.java
@@ -1,5 +1,6 @@
 package Temp;
 
+import Block.Brick;
 import Core.Board;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -7,6 +8,7 @@ import java.awt.Graphics;
 public class Bomb extends Temporary {
 
     private static int expRadius = 1;
+    private static boolean leftHit, rightHit, upHit, downHit;
 
     public Bomb(int x, int y, Board board) {
         super(x, y, 2000, board);
@@ -16,29 +18,53 @@ public class Bomb extends Temporary {
     public void isDone() {
         for (int i = 1; i <= expRadius; i++) {
             if (x + 1 - i > 0) {
-                if (board.getMatrixItem(x - i, y).isDestructible()) {
-                    board.setDrawable(new Explosion(x - i, y, board));
+                if (!leftHit) {
+                    if (board.getMatrixItem(x - i, y).isDestructible() ) {
+                        if (board.getMatrixItem(x - i, y) instanceof Brick) {
+                            leftHit = true;
+                        }
+                        board.setDrawable(new Explosion(x - i, y, board));
+                    }
                 }
             }
             if (x - 1 + i < board.getMatrix().length - 1) {
-                if (board.getMatrixItem(x + i, y).isDestructible()) {
-                    board.setDrawable(new Explosion(x + i, y, board));
+                if (!rightHit) {
+                    if (board.getMatrixItem(x + i, y).isDestructible()) {
+                        if (board.getMatrixItem(x + i, y) instanceof Brick) {
+                            rightHit = true;
+                        }
+                        board.setDrawable(new Explosion(x + i, y, board));
+                    }
                 }
             }
             if (board.getMatrixItem(x, y).isDestructible()) {
                 board.setDrawable(new Explosion(x, y, board));
             }
             if (y - 1 + i < board.getMatrix()[0].length - 1) {
-                if (board.getMatrixItem(x, y + i).isDestructible()) {
-                    board.setDrawable(new Explosion(x, y + i, board));
+                if (!downHit) {
+                    if (board.getMatrixItem(x, y + i).isDestructible()) {
+                        if (board.getMatrixItem(x, y + i) instanceof Brick) {
+                            downHit = true;
+                        }
+                        board.setDrawable(new Explosion(x, y + i, board));
+                    }
                 }
             }
             if (y + 1 - i > 0) {
-                if (board.getMatrixItem(x, y - i).isDestructible()) {
-                    board.setDrawable(new Explosion(x, y - i, board));
+                if (!upHit) {
+                    if (board.getMatrixItem(x, y - i).isDestructible()) {
+                        if (board.getMatrixItem(x, y - i) instanceof Brick) {
+                            upHit = true;
+                        }
+                        board.setDrawable(new Explosion(x, y - i, board));
+                    }
                 }
             }
         }
+        leftHit = false;
+        rightHit = false;
+        upHit = false;
+        downHit = false;
     }
 
     @Override
@@ -64,5 +90,5 @@ public class Bomb extends Temporary {
     public static void setExpRadius(int expRadius) {
         Bomb.expRadius += expRadius;
     }
-    
+
 }
